6fe8a0256d0434c1e641f4be13647aa369d797ca
keerthika-busetty <81732762+keerthika-busetty@users.noreply.github.com>
2021-07-20 23:14:29 +0800

    Add files via upload

---
+++ TradingReprot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TradingReprot;
+using TradingReprot.Interface;
+using Unity;
+
+namespace TradingReport
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //using Unity Application block for resolving IMessageBox for Unit Testing
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<IMessageBox, MessageBoxWrapper>();
+            
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(container.Resolve<Form1>());
+        }
+    }
+}
