bf8d23cd7b4ffa95aa964a66c2e43d4f9cdbc65b
keerthika-busetty <81732762+keerthika-busetty@users.noreply.github.com>
2021-07-21 16:48:40 +0800

    Add files via upload

---
+++ TradingReprot/ReportView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TradingReport.Data;
+using TradingReport.Interface;
+using TradingReprot.Interface;
+using Unity;
+
+namespace TradingReport
+{
+    public class ReportView  : IReportView
+    {
+        public IReport report;
+        public  bool GETReporObject(producttype type,string Broker)
+        {
+            IUnityContainer container = new UnityContainer();
+            
+            if (type == producttype.FXForwards)
+            { 
+              container.RegisterType<ILoad, FXForwardsData>();
+              
+            }
+
+            // var myObj = Activator.CreateInstance(Type.GetType("TradingReport.Broker" + "." + type+"Broker"+Broker), new object[] { new FXForwardsData() });
+            var myObj = Activator.CreateInstance(Type.GetType("TradingReport.Broker" + "." + type + "Broker" + Broker), new object[] { container.Resolve<ILoad>() });
+            IReport reportObj = (IReport)myObj;
+
+            report = reportObj;
+            report.GetData();
+           return  report.GenerateReport();
+        }
+    }
+}
