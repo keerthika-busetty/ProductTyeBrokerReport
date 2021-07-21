ad1684e59c51758992fb33ec2ceefeb6fa183945
keerthika-busetty <81732762+keerthika-busetty@users.noreply.github.com>
2021-07-21 16:47:22 +0800

    Add files via upload

---
+++ TradingReprot/Interface/IReportView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using TradingReport.Interface;
+using TradingReport;
+
+namespace TradingReprot.Interface
+{
+    
+     public interface IReportView
+    {
+        bool GETReporObject(producttype type, string Broker);
+        
+    }
+}
